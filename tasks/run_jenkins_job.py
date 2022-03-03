eaf67427be2721dffaf237da51e3b9417fb32aad
mihirpandya-greenops <84353489+mihirpandya-greenops@users.noreply.github.com>
2022-03-02 16:38:47 -0800

    Update run_jenkins_job.py

--- tasks/run_jenkins_job.py
+++ tasks/run_jenkins_job.py
@@ -7,13 +7,13 @@ import time
 jenkins_url = os.getenv('JENKINS_URL')
 print(jenkins_url)
 jenkins_username = 'admin' #os.getenv('JENKINS_USERNAME')
-jenkins_password = '1c92025660084d239a4e9a5e89c33d2d' #os.getenv('JENKINS_PASSWORD')
+jenkins_password = os.getenv('JENKINS_PASSWORD')
 job_name = os.getenv('JOB_NAME')
-token_name = os.getenv('TOKEN_NAME')
+# token_name = os.getenv('TOKEN_NAME')
 
 jenkins_srv = jenkins.Jenkins(jenkins_url, username=jenkins_username, password=jenkins_password)
 next_build_number = jenkins_srv.get_job_info(job_name)['nextBuildNumber']
-jenkins_srv.build_job(job_name, token=token_name)
+jenkins_srv.build_job(job_name)#, token=token_name)
 
 while True:
     time.sleep(10)
